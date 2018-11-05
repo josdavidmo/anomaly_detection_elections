d92de935e611457c91d34f209443f817bd306616
josdavidmo <josdavidmo@gmail.com>
2018-11-04 20:41:54 -0500

    Sliding method generalized and votation region API implemented

--- main.py
+++ main.py
@@ -1,97 +1,59 @@
 from pdf2image import convert_from_path
-from utils.algorithms import pyramid
-from utils.algorithms import sliding_window
-from utils.tensorflow import load_graph
-from utils.tensorflow import read_tensor_from_image
-from utils.tensorflow import load_labels
+from utils.algorithms import computational_vision
+from utils.algorithms import WIN_W,WIN_H
+from utils.algorithms import get_zone_region
 import argparse
 import imutils
-import time
-import cv2
 import numpy as np
-import multiprocessing
-import tensorflow as tf
-
-CHARACTER_DETECTION = 'models/character_detection.pb'
-CHARACTER_CLASSIFICATION = 'models/character_classification.pb'
-CHARACTER_SEGMENTATION = 'models/character_segmentation.pb'
-
-LABEL_CHARACTER_DETECTION = "models/character_detection_labels.txt"
-LABEL_CHARACTER_CLASSIFICATION = "models/character_classification_labels.txt"
-LABEL_CHARACTER_SEGMENTATION = "models/character_segmentation_labels.txt"
-
-INPUT_HEIGHT = 224
-INPUT_WIDTH = 224
-INPUT_MEAN = 128
-INPUT_STD = 128
-INPUT_LAYER = "input"
-OUTPUT_LAYER = "final_result"
+import time
+import pprint
 
 
-def image_recognition(args):
-    window, x, y, w, h = args
-    # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-    # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-    # WINDOW
-    t = read_tensor_from_image(window,
-                               input_height=INPUT_HEIGHT,
-                               input_width=INPUT_WIDTH,
-                               input_mean=INPUT_MEAN,
-                               input_std=INPUT_STD)
+MODELS = {
+    "region_detection": {
+        "model": "models/region_detection.pb",
+        "labels": "models/region_detection_labels.txt"
+    },
+    "character_detection": {
+        "model": "models/character_detection.pb",
+        "labels": "models/character_detection_labels.txt"
+    },
+    "character_classification": {
+        "model": "models/character_classification.pb",
+        "labels": "models/character_classification_labels.txt"
+    },
+    "character_segmentation": {
+        "model": "models/character_segmentation.pb",
+        "labels": "models/character_segmentation_labels.txt"
+    },
+}
 
-    input_name = "import/" + INPUT_LAYER
-    output_name = "import/" + OUTPUT_LAYER
-    input_operation = graph.get_operation_by_name(input_name)
-    output_operation = graph.get_operation_by_name(output_name)
-
-    with tf.Session(graph=graph) as sess:
-        results = sess.run(output_operation.outputs[0],
-                           {input_operation.outputs[0]: t})
-    results = np.squeeze(results)
-
-    top_k = results.argsort()[-5:][::-1]
-    labels = load_labels(LABEL_CHARACTER_DETECTION)
-
-    if labels[top_k[0]] == 'an':
-        return (x * w, y * h)
-    return None
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="Path to the image")
     ap.add_argument("-w", "--workers", required=True, help="Workers")
+
     args = vars(ap.parse_args())
+    workers = int(args["workers"])
     image = convert_from_path(args["image"])[0].convert('RGB')
     image = np.array(image)
-    scale = 5
+
+    votation_region = get_zone_region(image)
+
+    scale = 10
     w = int(image.shape[1] / scale)
     image = imutils.resize(image, width=w)
-    # define the window width and height
-    (winW, winH) = (34, 39)
 
-    ghetto_queue = []
 
-    # load model
-    graph = load_graph(CHARACTER_DETECTION)
-
-    # loop over the image pyramid
-    for (resized, w, h) in pyramid(image, scale=1.5):
-        # loop over the sliding window for each layer of the pyramid
-        for (x, y, window) in sliding_window(resized, stepSizeX=17, stepSizeY=19, windowSize=(winW, winH)):
-
-            # if the window does not meet our desired window size, ignore it
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
-            ghetto_queue.append((window, x, y, w, h))
-
-    pool = multiprocessing.Pool(int(args["workers"]))
-    tasks = pool.map(image_recognition, ghetto_queue)
-    character_detection_array = filter(None, tasks)
-    for (x, y) in character_detection_array:
-        cv2.rectangle(image, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-    cv2.namedWindow("Window", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow("Window", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imwrite("result.png", image)
+    # region_detection_array = computational_vision(image, MODELS["region_detection"],
+    #             ('cubic','title'), workers)
+    #
+    # for (x, y) in region_detection_array:
+    #     cv2.rectangle(image, (x, y), (x + WIN_W, y + WIN_H), (0, 255, 0), 2)
+    # cv2.namedWindow("Window", cv2.WINDOW_AUTOSIZE)
+    # cv2.imshow("Window", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.imwrite("result.png", image)
--- utils/algorithms.py
+++ utils/algorithms.py
@@ -1,6 +1,44 @@
-import imutils
+from __future__ import absolute_import
 
-def pyramid(image, scale=1.5, minSize=(30, 30)):
+import cv2
+import imutils
+import multiprocessing
+import numpy as np
+import tensorflow as tf
+from utils.tensorflow import load_graph
+from utils.tensorflow import load_labels
+from utils.tensorflow import read_tensor_from_image
+
+INPUT_HEIGHT = 224
+INPUT_WIDTH = 224
+INPUT_MEAN = 128
+INPUT_STD = 128
+WIN_W=15
+WIN_H=15
+STEP_SIZE_X=7
+STEP_SIZE_Y=7
+SCALE=1.5
+THRESHOLD = .8
+INPUT_LAYER = "input"
+OUTPUT_LAYER = "final_result"
+TITLE_PATH = '/home/andrea/Proyectos/anomaly_detection_elections_project/anomaly_detection_elections/utils/0b0854bc-24b2-4a4a-8371-ae3aa1ab358a.png'
+CUBIC_PATH = '/home/andrea/Proyectos/anomaly_detection_elections_project/anomaly_detection_elections/utils/1b318ef1-bdbd-47b0-8bef-c274b7f89b5b.png'
+
+
+def get_zone_region(image):
+    title = cv2.imread(TITLE_PATH)
+    cubic = cv2.imread(CUBIC_PATH)
+    w_title, h_title = title.shape[:-1]
+    w_cubic, h_cubic = cubic.shape[:-1]
+
+    res_title = cv2.matchTemplate(image, title, cv2.TM_CCOEFF_NORMED)
+    res_cubic = cv2.matchTemplate(image, cubic, cv2.TM_CCOEFF_NORMED)
+
+    loc_title = zip(*np.where(res_title >= THRESHOLD)[::-1])[0]
+    loc_cubic = max(zip(*np.where(res_cubic >= THRESHOLD)[::-1]))
+    return image[loc_title[1] + h_title:loc_cubic[1],loc_title[0]:loc_cubic[0] + w_cubic,:]
+
+def pyramid(image, scale=SCALE, minSize=(30, 30)):
     # yield the original image
     yield (image, 1, 1)
 
@@ -26,3 +64,58 @@ def sliding_window(image, stepSizeX, stepSizeY, windowSize):
         for x in range(0, image.shape[1], stepSizeX):
             # yield the current window
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+
+def image_recognition(args):
+    window, x, y, w, h, path_graph, path_labels, match = args
+    # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+    # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+    # WINDOW
+
+    # load model
+    graph = load_graph(path_graph)
+    labels = load_labels(path_labels)
+
+    t = read_tensor_from_image(window,
+                               input_height=INPUT_HEIGHT,
+                               input_width=INPUT_WIDTH,
+                               input_mean=INPUT_MEAN,
+                               input_std=INPUT_STD)
+
+    input_name = "import/" + INPUT_LAYER
+    output_name = "import/" + OUTPUT_LAYER
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
+
+    with tf.Session(graph=graph) as sess:
+        results = sess.run(output_operation.outputs[0],
+                           {input_operation.outputs[0]: t})
+    results = np.squeeze(results)
+
+    top_k = results.argsort()[-5:][::-1]
+
+    if labels[top_k[0]] in match:
+        print (x * w, y * h)
+        return (x * w, y * h)
+    return None
+
+
+def computational_vision(image, model, match, workers, win_w=WIN_W, win_h=WIN_H,
+                         step_size_x=STEP_SIZE_X, step_size_y=STEP_SIZE_Y, scale=SCALE):
+    ghetto_queue = []
+
+    # loop over the image pyramid
+    for (resized, w, h) in pyramid(image, scale):
+        # loop over the sliding window for each layer of the pyramid
+        for (x, y, window) in sliding_window(resized, step_size_x, step_size_y,
+                                             windowSize=(win_w, win_h)):
+
+            # if the window does not meet our desired window size, ignore it
+            if window.shape[0] != win_h or window.shape[1] != win_w:
+                continue
+            ghetto_queue.append((window, x, y, w, h, model["model"],
+                    model["labels"], match))
+
+    pool = multiprocessing.Pool(workers)
+    tasks = pool.map(image_recognition, ghetto_queue)
+    return filter(None, tasks)
