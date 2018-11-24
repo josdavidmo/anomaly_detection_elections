95d4b745406ee027fe82f939e77951000ee91eef
José David Moreno Posada <josdavidmo@gmail.com>
2018-11-24 13:40:38 -0500

    Votes summatory

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ from utils.algorithms import (MARGIN_X, MARGIN_Y, PADDING, WIN_H, WIN_W,
                               computational_vision, get_cells,
                               get_crop_coordinates, get_zone_region)
 
-MODELS = {
-    "character_classification": {
-        "model": "models/v2/character_classification.pb",
-        "labels": "models/v2/character_classification_labels.txt"
-    }
-}
-
 logger = verboselogs.VerboseLogger(__name__)
 logger.addHandler(logging.StreamHandler())
+
+CANDIDATES = ["GUSTAVO PETRO",
+              "PROMOTORES VOTO EN BLANCO",
+              "IVÁN DUQUE",
+              "HUMBERTO DE LA CALLE",
+              "JORGE ANTONIO TRUJILLO SARMIENTO",
+              "SERGIO FAJARDO",
+              "VIVIANE MORALES",
+              "GERMÁN VARGAS LLERAS"]
+
+TOTAL_LABELS = ["VOTOS EN BLANCO",
+                "VOTOS NULOS",
+                "VOTOS NO MARCADOS",
+                "TOTAL VOTOS DE LA MESA"]
+
+LABELS = CANDIDATES + TOTAL_LABELS
 
 if __name__ == "__main__":
     workers = multiprocessing.cpu_count() + 1
@@ -31,7 +40,7 @@ if __name__ == "__main__":
     ap.add_argument("-w", "--workers",
                     default=workers, help="Number of workers")
     ap.add_argument("-v", "--verbosity",
-                    default=1, help="Configure logger for requested verbosity")
+                    default=2, help="Configure logger for requested verbosity")
     args = vars(ap.parse_args())
     verbosity = int(args["verbosity"])
 
@@ -43,7 +52,7 @@ if __name__ == "__main__":
     elif verbosity >= 2:
         logger.setLevel(logging.VERBOSE)
     elif verbosity >= 1:
-        logger.setLevel(logging.NOTICE)
+        logger.setLevel(logging.INFO)
     elif verbosity < 0:
         logger.setLevel(logging.WARNING)
 
@@ -53,7 +62,7 @@ if __name__ == "__main__":
     image = convert_from_path(args["image"])[0].convert('RGB')
     image = np.array(image)
     end = time.time()
-    logger.debug("Evaluation time (1-image): {:.3f}s".format(end - start))
+    logger.debug("Evaluation time : {:.3f}s".format(end - start))
     if verbosity > 2:
         cv2.imshow("Window", image)
         cv2.waitKey(1)
@@ -63,7 +72,7 @@ if __name__ == "__main__":
     start = time.time()
     vote_region = get_zone_region(image)
     end = time.time()
-    logger.debug("Evaluation time (1-image): {:.3f}s".format(end - start))
+    logger.debug("Evaluation time : {:.3f}s".format(end - start))
     if verbosity > 2:
         cv2.imshow("Window", vote_region)
         cv2.waitKey(1)
@@ -76,26 +85,45 @@ if __name__ == "__main__":
         clone = vote_region.copy()
         for (x, y, step_size_x, step_size_y) in points:
             cv2.rectangle(clone,
-                              (x, y),
-                              (x + step_size_x,
-                               y + step_size_y),
-                              (0, 255, 0),
-                              2)
+                          (x, y),
+                          (x + step_size_x,
+                           y + step_size_y),
+                          (0, 255, 0),
+                          2)
         cv2.imshow("Window", clone)
         cv2.waitKey(1)
         cv2.imwrite("results/vote_region_segmented.png", clone)
 
     logger.info("Character Classification")
-    for (cell) in get_cells(vote_region, points):
+    results = []
+    candidate_result = ""
+    for (i, cell) in enumerate(get_cells(vote_region, points)):
         start = time.time()
-        cell = cv2.resize(cell, (WIN_W,WIN_H), interpolation = cv2.INTER_AREA)
-        result = computational_vision(cell)
-
-        #cv2.imwrite("/home/josdavidmo/Projects/character_classification_model/tf_files/character_clasification/%s/%s.png"
-        #            % (result,str(uuid.uuid4())), cell)
+        cell = cv2.resize(cell, (WIN_W, WIN_H), interpolation=cv2.INTER_AREA)
+        try:
+            result = computational_vision(cell)
+            if i % 3 == 0 and i != 0:
+                number_votes = 0
+                if candidate_result != '':
+                    number_votes = int(candidate_result)
+                results.append(number_votes)
+                candidate_result = result[0] if result[0] not in \
+                    ('dash', 'slash', 'white') else ''
+            else:
+                if result[0] not in ('dash', 'slash'):
+                    candidate_result += result[0]
+        except ValueError as e:
+            logger.error(e)
         end = time.time()
-        logger.info("Result: {}".format(result))
-        logger.debug("Evaluation time (1-image): {:.3f}s".format(end - start))
+        logger.debug("Value : {}".format(result))
+        logger.debug("Evaluation time : {:.3f}s".format(end - start))
         if verbosity > 2:
             cv2.imshow("Result: {}".format(result), cell)
             cv2.waitKey(0)
+    results.append(int(candidate_result) if candidate_result != '' else 0)
+    logger.info("--------------------------------------------------")
+    for i, result in enumerate(results):
+        logger.info("{0:40} {1:3d}".format(LABELS[i], results[i]))
+    if results[-1] != sum(results[:-1]):
+        logger.warning("the sum does not match")
+    logger.info("--------------------------------------------------")
--- sample_data.py
+++ sample_data.py
@@ -8,6 +8,7 @@ import os
 import cv2
 import imutils
 import numpy as np
+import verboselogs
 from pdf2image import convert_from_path
 
 import MySQLdb
@@ -19,16 +20,20 @@ HOST = 'localhost'
 USER = 'root'
 PASSWORD = 'root'
 DB = 'registraduria'
-BATCH_SIZE = 100
+BATCH_SIZE = 50
 PATH = "/home/josdavidmo/Projects/character_classification_model/tf_files/character_clasification/%s"
 RESULT_PATH = "/" + PATH + "/%s.png"
 FILE_PATH = "../data/%s/%s/%s/%s/%s.pdf"
-LOG_FILE = open("logger.txt","a")
+LOG_FILE = open("logger.log", "a")
+
+logger = verboselogs.VerboseLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
 
 def extract_data(args):
     start, length = args
-
-    print("Extracting from %s to %s" % (start, (start + length)))
+    start += 10000
+    logger.info("Extracting from %s to %s" % (start, (start + length)))
     db = MySQLdb.connect(HOST, USER, PASSWORD, DB)
     cursor = db.cursor()
     query = "SELECT * FROM mesa LIMIT %s, %s" % (
@@ -45,10 +50,11 @@ def extract_data(args):
             cell = cv2.resize(cell, (WIN_W, WIN_H),
                               interpolation=cv2.INTER_AREA)
             result, probability = computational_vision(cell)
+            id_image = str(uuid.uuid4())
             if len(os.listdir(PATH % (result))) <= 500:
-                id_image = str(uuid.uuid4())
                 cv2.imwrite(RESULT_PATH % (result, id_image), cell)
-                LOG_FILE.write("%s: %s - %s\n" % (id_image,file,str(probability)))
+            LOG_FILE.write("%s: %s - %s\n" %
+                           (id_image, file, str(probability)))
     return True
 
 
@@ -56,18 +62,32 @@ if __name__ == "__main__":
     workers = multiprocessing.cpu_count() + 1
     ap = argparse.ArgumentParser()
     ap.add_argument("-w", "--workers",
-                    default=3, help="Number of workers")
+                    default=workers, help="Number of workers")
     ap.add_argument("-b", "--batch_size",
                     default=BATCH_SIZE, help="Batch size")
+    ap.add_argument("-v", "--verbosity",
+                    default=2, help="Configure logger for requested verbosity")
     args = vars(ap.parse_args())
-    workers = int(args["workers"])
+    verbosity = int(args["verbosity"])
 
+    # Configure logger for requested verbosity.
+    if verbosity >= 4:
+        logger.setLevel(logging.SPAM)
+    elif verbosity >= 3:
+        logger.setLevel(logging.DEBUG)
+    elif verbosity >= 2:
+        logger.setLevel(logging.VERBOSE)
+    elif verbosity >= 1:
+        logger.setLevel(logging.INFO)
+    elif verbosity < 0:
+        logger.setLevel(logging.WARNING)
 
-    total = 2000
+    total = 1500
     args_queue = []
     for start in range(0, total, BATCH_SIZE):
         length = min(BATCH_SIZE, total - start)
         args_queue.append((start, length))
+    workers = int(args["workers"])
     pool = multiprocessing.Pool(workers)
     pool.map(extract_data, args_queue)
     pool.close()
